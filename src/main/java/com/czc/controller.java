e78b08873c7950810ce3603990d40ac4eec2f425
czc <1875587966@qq.com>
2022-08-17 16:13:48 +0800

    czc-hot-fix3.0

--- src/main/java/com/czc/controller.java
+++ src/main/java/com/czc/controller.java
@@ -4,5 +4,6 @@ public class controller {
     public static void main(String[] args) {
         System.out.println("hello,word!");
         System.out.println("test2");
+        System.out.println("hot-fix");
     }
 }
